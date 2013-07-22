481ad787914884cea0c5719d48d76a9128ec1b47
Yannick Schutz <yannick.schutz@gmail.com>
2013-07-22 10:34:58 +0200

    New date format

--- utils.go
+++ utils.go
@@ -7,7 +7,10 @@ import (
 func toTime(timeString Date) time.Time {
   val, err := time.Parse("2006-01-02T15:04:05-07:00", string(timeString))
   if err != nil {
-    val, _ = time.Parse("Mon, 02 Jan 2006 15:04:05 -0700", string(timeString))
+    val, err = time.Parse("Mon, 02 Jan 2006 15:04:05 -0700", string(timeString))
+  }
+  if err != nil {
+    val, err = time.Parse("Mon, 02 Jan 2006 15:04:05 MST", string(timeString))
   }
   return val
 }
